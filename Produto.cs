e5000f82fa00b14b2b00b7c4a5e0d2ad81e23877
kaliel <kalielsoares6@gmail.com>
2024-10-29 21:06:35 -0300

    Adicionar arquivos de projeto.

---
+++ Produto.cs
@@ -0,0 +1,32 @@
+﻿namespace fixingPOO {
+    class Produto {
+        public string Name;
+        public double Price;
+        public int Amount;
+        
+
+        public void addProdut(int a) {
+            Amount += a; 
+        }
+
+        public void removeProdut(int a) {
+            Amount -= a;
+        }
+
+        public double amountInStorage() {
+            return Amount;
+        }
+
+        public double valueAmount() {
+            return Price * Amount;
+        }
+
+        public override string ToString() {
+            return Name 
+                + ", $" + 
+                Price.ToString("F2") + " O valor total em estoque é de: " 
+                + valueAmount();
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace fixingPOO {
+    class Program{
+
+        static void Main(string[] args ){
+            Produto A;
+
+            A = new Produto();
+
+            Console.WriteLine("Cadastro de Produto");
+            Console.WriteLine("Qual o nome do produto?");
+            A.Name = Console.ReadLine();
+            Console.WriteLine("Qual o valor dele?");
+            A.Price = double.Parse(Console.ReadLine());
+            Console.WriteLine("Vamos adicionar quanto em estoque?");
+            A.Amount = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(A);
+            
+            Console.WriteLine("Deseja fazer alguma alteração no Produto?");
+            char answer = char.Parse(Console.ReadLine());
+
+            while (answer == 's') {
+                Console.WriteLine("Vamos:\n1 - Adicionar mais produtos em estoque\n2 - Remover produtos do estoque.");
+                byte answerL = byte.Parse(Console.ReadLine()); 
+                switch (answerL) {
+                    case 1:
+                        Console.WriteLine("Vamos adicionar quantos produtos?");
+                        int add = int.Parse(Console.ReadLine());
+                        A.addProdut(add);
+                        break;
+                    case 2:
+                        Console.WriteLine("Vamos remover quantos produtos?");
+                        int remove = int.Parse(Console.ReadLine());
+                        A.removeProdut(remove);
+                        break;
+                }
+
+                Console.WriteLine("Deseja fazer alguma alteração no Produto?");
+                answer = char.Parse(Console.ReadLine());
+            }
+
+
+
+            Console.WriteLine(A);
+
+        }
+       
+    }
+}
+
